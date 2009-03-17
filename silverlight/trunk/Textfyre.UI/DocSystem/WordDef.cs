444342eff0ac020997048ca8c71947d276141b03
Thomas Lynge <thomaslynge@users.noreply.github.com>
2009-03-17 21:40:42 +0000

    WordDef class added.

---
+++ silverlight/trunk/Textfyre.UI/DocSystem/WordDef.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Textfyre.UI.DocSystem
+{
+    public class WordDef
+    {
+        public string Word;
+        public Point PointBegin;
+        public Point PointEnd;
+        public Run Run;
+
+        public static System.Collections.Generic.Dictionary<string, string> WordDefs;
+
+        public static string Key(string word)
+        {
+            return word.Trim().ToLower().Replace(" ", "_");
+        }
+
+        /// <summary>
+        /// Parse an entire text for WordDefs and return a new text to replace the old one.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string ParseTextForWordDefs(string text)
+        {
+
+            if (WordDefs == null)
+            {
+                InitWordDefs();
+            }
+
+            foreach (string word in WordDefs.Keys)
+            {
+                text = Regex.Replace(text, @"\b" + word + @"\b", @"<WordDef>$&</WordDef>", RegexOptions.IgnoreCase);
+            }
+
+            return text;
+        }
+
+        private static void InitWordDefs()
+        {
+            if (WordDefs != null)
+                return;
+
+            WordDefs = new System.Collections.Generic.Dictionary<string, string>();
+            XDocument x = XDocument.Load(Current.Application.GetResPath("GameFiles/WordDefinition.xml"));
+
+            var worddefs = from worddef in x.Descendants("WordDef") select worddef;
+
+            foreach (var worddef in worddefs)
+            {
+                var words = from word in worddef.Descendants("Word") select word;
+                foreach (var word in words)
+                {
+                    string wordVal = Key(word.Value);
+                    string description = worddef.Element("Description").Value;
+                    WordDefs.Add(wordVal, description);
+                }
+            }
+
+        }
+
+    }
+}
