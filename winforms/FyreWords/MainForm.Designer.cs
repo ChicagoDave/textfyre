50e6745298cbd0a1d33918b3094ead30dd655031
David Cornelson <chicagodave@users.noreply.github.com>
2009-06-06 04:18:30 +0000

    Added FyreWords, for editing the WordDefinition.xml files.

---
+++ winforms/FyreWords/MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace FyreWords {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.PrimaryWords = new System.Windows.Forms.ListBox();
+            this.Definition = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.WordList = new System.Windows.Forms.TextBox();
+            this.NewWord = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(437, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // PrimaryWords
+            // 
+            this.PrimaryWords.FormattingEnabled = true;
+            this.PrimaryWords.Location = new System.Drawing.Point(16, 51);
+            this.PrimaryWords.Name = "PrimaryWords";
+            this.PrimaryWords.Size = new System.Drawing.Size(200, 160);
+            this.PrimaryWords.TabIndex = 1;
+            this.PrimaryWords.SelectedIndexChanged += new System.EventHandler(this.PrimaryWords_SelectedIndexChanged);
+            // 
+            // Definition
+            // 
+            this.Definition.Location = new System.Drawing.Point(16, 230);
+            this.Definition.Multiline = true;
+            this.Definition.Name = "Definition";
+            this.Definition.Size = new System.Drawing.Size(406, 160);
+            this.Definition.TabIndex = 5;
+            this.Definition.Leave += new System.EventHandler(this.Definition_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primary Word";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Definition";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(347, 399);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.Text = "&Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(219, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Word List";
+            // 
+            // WordList
+            // 
+            this.WordList.Location = new System.Drawing.Point(222, 51);
+            this.WordList.Multiline = true;
+            this.WordList.Name = "WordList";
+            this.WordList.Size = new System.Drawing.Size(200, 160);
+            this.WordList.TabIndex = 9;
+            this.WordList.Leave += new System.EventHandler(this.WordList_Leave);
+            // 
+            // NewWord
+            // 
+            this.NewWord.Location = new System.Drawing.Point(378, 27);
+            this.NewWord.Name = "NewWord";
+            this.NewWord.Size = new System.Drawing.Size(43, 20);
+            this.NewWord.TabIndex = 10;
+            this.NewWord.Text = "New";
+            this.NewWord.UseVisualStyleBackColor = true;
+            this.NewWord.Click += new System.EventHandler(this.NewWord_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 434);
+            this.Controls.Add(this.NewWord);
+            this.Controls.Add(this.WordList);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Definition);
+            this.Controls.Add(this.PrimaryWords);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "FyreWords - Definition Manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ListBox PrimaryWords;
+        private System.Windows.Forms.TextBox Definition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox WordList;
+        private System.Windows.Forms.Button NewWord;
+    }
+}
+
---
+++ winforms/FyreWords/WordDefinition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FyreWords {
+    public class WordDefinition {
+        private string _primaryWord;
+        private List<string> _wordList = new List<string>();
+        private string _definition;
+
+        public WordDefinition() {
+        }
+
+        public string PrimaryWord {
+            get {
+                return _primaryWord;
+            }
+            set {
+                _primaryWord = value;
+            }
+        }
+
+        public List<string> WordList {
+            get {
+                return _wordList;
+            }
+            set {
+                _wordList = value;
+            }
+        }
+
+        public string Definition {
+            get {
+                return _definition;
+            }
+            set {
+                _definition = value;
+            }
+        }
+    }
+}
