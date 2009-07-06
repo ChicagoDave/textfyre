e7523736029a70f659b69408c723507b82b620ea
Thomas Lynge <thomaslynge@users.noreply.github.com>
2009-07-06 21:08:39 +0000

    Settings

--- silverlight/trunk/Textfyre.UI/Controls/UserSettings/UserSettingsForm.xaml.cs
+++ silverlight/trunk/Textfyre.UI/Controls/UserSettings/UserSettingsForm.xaml.cs
@@ -18,7 +18,20 @@ namespace Textfyre.UI.Controls.UserSettings
         {
             InitializeComponent();
             Current.Font.ApplyFont(Textfyre.UI.Current.Font.FontType.Header, DialogTitle);
-            DialogTitle.FontSize = 24d;        
+            DialogTitle.FontSize = 24d;
+
+            string fontFamily = Storage.Settings.Get("FontFamily");
+
+            if (fontFamily.Length == 0)
+                fontFamily = "Default";
+
+            DdlFont.Items.Add(new TextBlock().Text = "Default");
+
+            if( Current.Application.Platform == Textfyre.UI.Current.Platform.Windows )
+                DdlFont.Items.Add(new TextBlock().Text = "Georgia");
+
+            DdlFont.Items.Add(new TextBlock().Text = "Times New Roman");
+            
         }
 
         public void Show()
