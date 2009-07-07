0146328bd70e9612e78e446a5f6ee35c476448ba
tlynge <tlynge@26785b93-e585-0a44-950e-6e906213fcf0>
2009-07-07 10:30:41 +0000

    Added UserSettings and ColorPicker Control

---
+++ silverlight/trunk/Textfyre.UI/Controls/UserSettings/ColorChangedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Textfyre.UI.Controls.UserSettings
+{
+    public class ColorChangedEventArgs : RoutedEventArgs
+    {
+        internal ColorChangedEventArgs(Color selectedColor)
+        {
+            this.SelectedColor = selectedColor;
+        }
+
+        public Color SelectedColor
+        {
+            get;
+            private set;
+        }
+    }
+}
---
+++ silverlight/trunk/Textfyre.UI/Controls/UserSettings/ColorPickerButton.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Textfyre.UI.Controls.UserSettings
+{
+    public partial class ColorPickerButton : UserControl
+    {
+        public ColorPickerButton()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnColor_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
