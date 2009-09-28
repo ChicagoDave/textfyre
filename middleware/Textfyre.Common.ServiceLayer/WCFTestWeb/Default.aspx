144786d42f9b68aa962d93db0ad2d05debf70a1e
David Cornelson <chicagodave@users.noreply.github.com>
2009-09-28 05:13:14 +0000

    Finished middleware cleanup.

---
+++ middleware/Textfyre.Common.ServiceLayer/WCFTestWeb/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WCFTestWeb.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>        
+        <h1>Utilizing WCF Service</h1>
+        
+        <asp:UpdatePanel ID="up1" runat="server" UpdateMode="Conditional">
+            <ContentTemplate>
+                <asp:Label ID="lblCountries" runat="server" Text="Country:"></asp:Label>
+                &nbsp;
+                <asp:DropDownList ID="ddlCountries" runat="server" AutoPostBack="true" EnableViewState="false"></asp:DropDownList>
+                &nbsp;&nbsp;
+                <asp:Label runat="server" ID="lblCities" Text="City:"></asp:Label>
+                &nbsp;
+                <asp:DropDownList ID="ddlCities" runat="server" Enabled="False" EnableViewState="False"></asp:DropDownList>
+            </ContentTemplate>
+            <Triggers>
+                <asp:AsyncPostBackTrigger ControlID="ddlCountries" EventName="SelectedIndexChanged" />
+            </Triggers>
+        </asp:UpdatePanel>
+        <asp:Button ID="btnDisplay" runat="server" OnClick="btnDisplay_Click" Text="Display" />   
+        <asp:Label ID="lblDisplay" runat="server" Visible="false"></asp:Label>
+    </div>    
+    </form>
+</body>
+</html>
---
+++ middleware/Textfyre.Common.ServiceLayer/WCFTestWeb/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using System.ServiceModel;
+using IAAI.Common.Contracts;
+using System.Collections.Generic;
+
+namespace WCFTestWeb {
+    public partial class Default : System.Web.UI.Page {
+
+        protected void Page_Load(object sender, EventArgs e) {
+            PopulateCountries();
+
+            string country = Request.Form[ddlCountries.UniqueID];
+            UpdateSelection(ddlCountries, country);
+            if (ddlCountries.SelectedIndex != 0) {
+                PopulateCities(country);
+
+                string city = Request.Form[ddlCities.UniqueID];
+                UpdateSelection(ddlCities, city);
+            } else {
+                ddlCities.Items.Insert(0, new ListItem("Select a city...", String.Empty));
+            }
+        }
+
+        private void UpdateSelection(DropDownList ddl, string text) {
+            if(!String.IsNullOrEmpty(text)) {
+                foreach(ListItem item in ddl.Items) {                    
+                    if(item.Text == text) {
+                        item.Selected = true;
+                        break;
+                    }
+                }
+            }
+
+            if ((ddl.SelectedIndex == 0) && (ddl.Items.Count == 2)) {
+                ddl.Items[0].Selected = false;
+                ddl.Items[1].Selected = true;
+            }
+        }
+
+        private void PopulateCountries() {            
+            EndpointAddress epa = new EndpointAddress(@"http://localhost:3743/TimeZoneService.svc");
+            ITimeZoneService proxy = ChannelFactory<ITimeZoneService>.CreateChannel(new BasicHttpBinding(), epa);
+            List<string> countries = proxy.GetCountries();            
+
+            ddlCountries.DataSource = countries;
+            ddlCountries.DataBind();
+            ddlCountries.Items.Insert(0, new ListItem("Select a country...", String.Empty));
+        }
+
+        private void PopulateCities(string country) {
+            EndpointAddress epa = new EndpointAddress(@"http://localhost:3743/TimeZoneService.svc");
+            ITimeZoneService proxy = ChannelFactory<ITimeZoneService>.CreateChannel(new BasicHttpBinding(), epa);
+            Dictionary<string, int> cities = proxy.GetCities(country);
+
+            ddlCities.Enabled = true;
+            ddlCities.DataSource = cities;
+            ddlCities.DataTextField = "Key";
+            ddlCities.DataValueField = "Value";
+            ddlCities.DataBind();            
+            ddlCities.Items.Insert(0, "Select a city...");
+        }
+
+        protected void btnDisplay_Click(object sender, EventArgs e) {
+            if ((ddlCountries.SelectedIndex != 0) && (ddlCities.SelectedIndex != 0)) {
+                lblDisplay.Visible = true;
+                lblDisplay.Text = ddlCountries.SelectedItem.Text + "::" + ddlCities.SelectedItem.Text;
+            } else {
+                lblDisplay.Visible = false;
+            }
+        }
+    }
+}
