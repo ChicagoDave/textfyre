6fd76ee0d930631f931eb5711ea600fda2a3750b
dcornelson <dcornelson@26785b93-e585-0a44-950e-6e906213fcf0>
2009-10-23 02:34:36 +0000

    Added SandDock UI.

--- Silverfyre/Silverfyre.Web/Default.aspx.cs
+++ Silverfyre/Silverfyre.Web/Default.aspx.cs
@@ -5,13 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace SilverFyre.Web
-{
-	public partial class _Default : System.Web.UI.Page
-	{
-		protected void Page_Load( object sender, EventArgs e )
-		{
+namespace SilverFyre.Web {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
 
-		}
-	}
+        }
+    }
 }
