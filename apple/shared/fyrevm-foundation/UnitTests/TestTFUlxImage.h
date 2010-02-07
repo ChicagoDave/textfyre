a433ac94824a6fdf211872d344d1518928fab83c
Andrew Pontious <apontious@users.noreply.github.com>
2010-02-07 08:19:50 +0000

    Changes to make unit testing work in a uniform and correct manner: - Made LogicTests and ApplicationTests directories and targets for both platforms. - Made proper modifications to make Logic and App tests work for iPhone -- harder than I would like to get this stuff working properly. File 4 bugs on Apple sample code, documentation, and SDK functionality as part of this effort. But it all works now! Tomorrow, back to actually writing code.

--- apple/shared/fyrevm-foundation/UnitTests/TestTFUlxImage.h
+++ apple/shared/fyrevm-foundation/UnitTests/TestTFUlxImage.h
@@ -1,6 +1,6 @@
 //
 //  TestTFUlxImage.h
-//  fyrep-cocoa
+//  fyrevm-foundation
 //
 //  Copyright 2010 Textfyre, Inc. All rights reserved.
 //  Please read the accompanying COPYRIGHT file for licensing restrictions.
