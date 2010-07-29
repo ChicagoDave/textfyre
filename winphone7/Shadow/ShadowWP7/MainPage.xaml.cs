db5223163ec882641449faa9291b21c200ce0435
ccavanagh <ccavanagh@26785b93-e585-0a44-950e-6e906213fcf0>
2010-07-29 16:36:27 +0000

    Initial check-in of panorama-based mockup

--- winphone7/Shadow/ShadowWP7/MainPage.xaml.cs
+++ winphone7/Shadow/ShadowWP7/MainPage.xaml.cs
@@ -10,15 +10,195 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Textfyre.VM;
+using System.IO;
+using System.Reflection;
+using Cjc.SilverFyre;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading;
 
 namespace ShadowWP7
 {
-    public partial class MainPage : PhoneApplicationPage
+    public partial class MainPage : PhoneApplicationPage, INotifyPropertyChanged
     {
+		private EngineClient engine;
+		private AutoResetEvent saveLoadItemSelected = new AutoResetEvent( false );
+		private AutoResetEvent saveLoadCompleted = new AutoResetEvent( false );
+		private AutoResetEvent saveLoadCancelled = new AutoResetEvent( false );
+
+		private int? selectedCommandIndex = null;
+		private double? targetVerticalOffset = null;
+		private bool pinnedToEnd = false;
+
+		private string baseUrl;
+		private string storyUrl;
+
         // Constructor
         public MainPage()
         {
+			History = new ObservableCollection<StoryHistoryItem>();
+
             InitializeComponent();
+
+			engine = Load( "Lowell's Paradise.ulx" );
+
+			engine.OutputReady += engine_OutputReady;
         }
-    }
+
+		public string StoryTitle { get { return "Lowell's Paradise"; } }
+		public ObservableCollection<StoryHistoryItem> History { get; private set; }
+		public StoryState CurrentState { get; private set; }
+
+		void engine_OutputReady( object sender, OutputReadyEventArgs e )
+		{
+			if ( e.Package.Count > 0 )
+			{
+				Dispatcher.BeginInvoke( delegate
+				{
+					pleaseWait.Visibility = Visibility.Collapsed;
+					pleaseWait.Opacity = 0;
+					AddHistory( new StoryHistoryItem( null, e ) );
+				} );
+			}
+		}
+
+		private void ScrollToEnd()
+		{
+//			storyScroll.UpdateLayout();
+
+//			if ( pinnedToEnd ) storyScroll.ScrollToVerticalOffset( double.MaxValue );
+//			else targetVerticalOffset = storyScroll.ScrollableHeight;
+		}
+
+		private EngineClient Load( string filename )
+		{
+			using ( var story = Assembly.GetExecutingAssembly().GetManifestResourceStream( "ShadowWP7." + filename ) )
+			{
+				return new EngineClient( story );
+			}
+		}
+
+		private void AddHistory( StoryHistoryItem item )
+		{
+//			pinnedToEnd = false;
+			History.Add( item );
+
+			if ( item.OutputArgs != null )
+			{
+				CurrentState = new StoryState( item.OutputArgs );
+				RaisePropertyChanged( "CurrentState" );
+			}
+		}
+
+		private Storyboard FindStoryboard( string name )
+		{
+			return (Storyboard)Resources[ name ];
+		}
+
+		private void OnParagraphLoaded( object sender, RoutedEventArgs e )
+		{
+			var textBlock = sender as TextBlock;
+			var paragraph = textBlock.DataContext as Paragraph;
+
+			foreach ( var inline in paragraph.Inlines ) textBlock.Inlines.Add( inline );
+		}
+
+		private void OnCommand( object sender, RoutedEventArgs e )
+		{
+/*			AddHistory( new StoryHistoryItem( commandBox.Text, null ) );
+
+			FindStoryboard( "showPleaseWait" ).Begin();
+			pleaseWait.Visibility = Visibility.Visible;
+
+			if ( engine != null ) engine.SendLine( commandBox.Text );
+			commandBox.Text = "";*/
+		}
+
+		private void OnCommandKey( object sender, KeyEventArgs e )
+		{
+			switch ( e.Key )
+			{
+				case Key.Enter:
+					{
+						OnCommand( sender, null );
+						break;
+					}
+
+				case Key.Up:
+					{
+						do
+						{
+							if ( !selectedCommandIndex.HasValue ) selectedCommandIndex = History.Count - 1;
+							else if ( selectedCommandIndex >= 0 ) --selectedCommandIndex;
+						}
+						while ( selectedCommandIndex >= 0 && !History[ selectedCommandIndex.Value ].HasInput );
+
+/*						commandBox.Text = ( selectedCommandIndex >= 0 )
+							? History[ selectedCommandIndex.Value ].Input
+							: "";
+						*/
+						e.Handled = true;
+						break;
+					}
+
+				case Key.Down:
+					{
+						do
+						{
+							if ( !selectedCommandIndex.HasValue ) selectedCommandIndex = 0;
+							else if ( selectedCommandIndex < History.Count ) ++selectedCommandIndex;
+						}
+						while ( selectedCommandIndex < History.Count && !History[ selectedCommandIndex.Value ].HasInput );
+
+/*						commandBox.Text = ( selectedCommandIndex < History.Count )
+							? History[ selectedCommandIndex.Value ].Input
+							: "";*/
+
+						e.Handled = true;
+						break;
+					}
+
+				default:
+					{
+						selectedCommandIndex = null;
+						if ( engine != null ) engine.SendKey( (char)( e.PlatformKeyCode ) );
+						break;
+					}
+			}
+
+			ScrollToEnd();
+		}
+
+		private void OnSaveLoadHidden( object sender, EventArgs e )
+		{
+//			saveLoadPopup.Visibility = Visibility.Collapsed;
+		}
+
+		private void OnLoadingHidden( object sender, EventArgs e )
+		{
+//			loading.Visibility = Visibility.Collapsed;
+		}
+
+		private void OnSaveLoadItemSelected( object sender, EventArgs e )
+		{
+//			saveLoadItemSelected.Set();
+		}
+
+		private void OnSaveLoadCancelled( object sender, EventArgs e )
+		{
+//			saveLoadCancelled.Set();
+		}
+
+		#region INotifyPropertyChanged Members
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void RaisePropertyChanged( string propertyName )
+		{
+			if ( PropertyChanged != null ) PropertyChanged( this, new PropertyChangedEventArgs( propertyName ) );
+		}
+
+		#endregion
+	}
 }
---
+++ winphone7/Shadow/ShadowWP7/SilverFyre/EngineClient.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Textfyre.VM;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+
+namespace Cjc.SilverFyre
+{
+	public class EngineClient
+	{
+		public event OutputReadyEventHandler OutputReady;
+		public event SaveRestoreEventHandler SaveRequested;
+		public event SaveRestoreEventHandler LoadRequested;
+
+		private MemoryStream game;
+		private Thread thread;
+		private Engine engine;
+
+		private string nextLine;
+		private char nextKey;
+
+		private AutoResetEvent keyReady = new AutoResetEvent( false );
+		private AutoResetEvent lineReady = new AutoResetEvent( false );
+		private ManualResetEvent stop = new ManualResetEvent( false );
+
+		private EventWaitHandle[] lineEvents;
+		private EventWaitHandle[] keyEvents;
+
+		public EngineClient( Stream gameFile )
+		{
+			lineEvents = new EventWaitHandle[] { lineReady, stop };
+			keyEvents = new EventWaitHandle[] { keyReady, stop };
+
+			var buffer = new byte[ gameFile.Length ];
+			gameFile.Read( buffer, 0, buffer.Length );
+
+			try
+			{
+				var blorb = Blorb.FromStream( new MemoryStream( buffer ) );
+				game = new MemoryStream( blorb[ Blorb.GLUL ] );
+			}
+			catch
+			{
+				game = new MemoryStream( buffer );
+			}
+
+			thread = new Thread( Run );
+			thread.Start();
+		}
+
+		public void Stop()
+		{
+		}
+
+		public void SendLine( string line )
+		{
+			nextLine = line;
+			lineReady.Set();
+		}
+
+		public void SendKey( char key )
+		{
+			nextKey = key;
+			keyReady.Set();
+		}
+
+		private void Run()
+		{
+			try
+			{
+				engine = new Engine( game );
+
+				engine.OutputReady += new OutputReadyEventHandler( engine_OutputReady );
+				engine.KeyWanted += new KeyWantedEventHandler( engine_KeyWanted );
+				engine.LineWanted += new LineWantedEventHandler( engine_LineWanted );
+				engine.SaveRequested += new SaveRestoreEventHandler( engine_SaveRequested );
+				engine.LoadRequested += new SaveRestoreEventHandler( engine_LoadRequested );
+
+				while ( !stop.WaitOne( 0 ) )
+				{
+					var started = DateTime.Now;
+
+					try
+					{
+						engine.Run();
+					}
+					catch ( Exception ex )
+					{
+						OutputError( ex );
+					}
+
+					// If game exited/failed immediately, take a breather...
+					if ( ( DateTime.Now - started ).TotalMilliseconds < 1000 ) Thread.Sleep( 3000 );
+				}
+			}
+			catch ( Exception ex )
+			{
+				OutputError( ex );
+			}
+		}
+
+		private void OutputError( Exception ex )
+		{
+			if ( OutputReady != null )
+			{
+				var package = new Dictionary<OutputChannel, string>();
+				package[ OutputChannel.Death ] = ex.Message;
+
+				OutputReady( this, new OutputReadyEventArgs { Package = package } );
+			}
+		}
+
+		void engine_KeyWanted( object sender, KeyWantedEventArgs e )
+		{
+			keyReady.Reset();
+
+			if ( WaitHandle.WaitAny( keyEvents ) == 0 ) e.Char = nextKey;
+			else engine.Stop();
+		}
+
+		void engine_LineWanted( object sender, LineWantedEventArgs e )
+		{
+			lineReady.Reset();
+
+			if ( WaitHandle.WaitAny( lineEvents ) == 0 ) e.Line = nextLine;
+			else engine.Stop();
+		}
+
+		void engine_OutputReady( object sender, OutputReadyEventArgs e )
+		{
+			if ( OutputReady != null ) OutputReady( this, e );
+		}
+
+		void engine_SaveRequested( object sender, SaveRestoreEventArgs e )
+		{
+			if ( SaveRequested != null ) SaveRequested( this, e );
+		}
+
+		void engine_LoadRequested( object sender, SaveRestoreEventArgs e )
+		{
+			if ( LoadRequested != null ) LoadRequested( this, e );
+		}
+	}
+}
