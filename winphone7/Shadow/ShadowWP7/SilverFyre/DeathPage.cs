27a614634602d54298356a90df48b33080ff5c01
Chris Cavanagh <blog@chriscavanagh.com>
2010-08-16 06:49:47 +0000

    Added some missing files

---
+++ winphone7/Shadow/ShadowWP7/SilverFyre/DeathPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Textfyre.VM;
+
+namespace Cjc.SilverFyre
+{
+	public class DeathPage : PageBase
+	{
+		public override bool HasInput { get { return true; } }
+
+		public DeathPage( StoryHistoryItem storyHistoryItem )
+			: base( storyHistoryItem, OutputChannel.Death )
+		{
+		}
+	}
+}
