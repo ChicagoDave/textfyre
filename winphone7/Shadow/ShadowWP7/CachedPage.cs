991af92c509dd878247b8cc822b7c2d5509a2ffc
ccavanagh <ccavanagh@26785b93-e585-0a44-950e-6e906213fcf0>
2010-09-05 07:06:40 +0000

    Added CachedPage

---
+++ winphone7/Shadow/ShadowWP7/CachedPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+
+namespace ShadowWP7
+{
+	public class CachedPage
+	{
+		public WriteableBitmap Bitmap { get; set; }
+		public Rect[] ParagraphBounds { get; set; }
+	}
+}
