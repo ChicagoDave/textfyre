8f78690b1f675b25f907f033fcbf86c2ca3924d5
David Cornelson <chicagodave@users.noreply.github.com>
2010-10-18 05:51:07 +0000

    Added intro doc project.

---
+++ winphone7/Shadow/IntroHelp/IntroHelp/IntroPage2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace IntroHelp
+{
+	public partial class IntroPage2 : UserControl
+	{
+		public IntroPage2()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
