705d436cdcd881a583deb45e35c39b2315a8247e
David Cornelson <chicagodave@users.noreply.github.com>
2010-11-22 03:54:58 +0000

    Adding FyreUnit solution and project.

---
+++ FyreUnit/FyreUnit/MainForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace FyreUnit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExitProgram = new System.Windows.Forms.ToolStripMenuItem();
+            this.OutputTextbox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(832, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFileMenu,
+            this.toolStripMenuItem1,
+            this.ExitProgram});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // OpenFileMenu
+            // 
+            this.OpenFileMenu.Name = "OpenFileMenu";
+            this.OpenFileMenu.Size = new System.Drawing.Size(103, 22);
+            this.OpenFileMenu.Text = "&Open";
+            this.OpenFileMenu.Click += new System.EventHandler(this.OpenFileMenu_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // ExitProgram
+            // 
+            this.ExitProgram.Name = "ExitProgram";
+            this.ExitProgram.Size = new System.Drawing.Size(103, 22);
+            this.ExitProgram.Text = "E&xit";
+            // 
+            // OutputTextbox
+            // 
+            this.OutputTextbox.Location = new System.Drawing.Point(464, 27);
+            this.OutputTextbox.Name = "OutputTextbox";
+            this.OutputTextbox.Size = new System.Drawing.Size(356, 380);
+            this.OutputTextbox.TabIndex = 4;
+            this.OutputTextbox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 419);
+            this.Controls.Add(this.OutputTextbox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "FyreUnit";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenFileMenu;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ExitProgram;
+        private System.Windows.Forms.RichTextBox OutputTextbox;
+    }
+}
+
---
+++ FyreUnit/FyreUnit/ProjectListForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FyreUnit
+{
+    partial class ProjectListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProjectListbox = new System.Windows.Forms.ListBox();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProjectListbox
+            // 
+            this.ProjectListbox.FormattingEnabled = true;
+            this.ProjectListbox.Location = new System.Drawing.Point(13, 13);
+            this.ProjectListbox.Name = "ProjectListbox";
+            this.ProjectListbox.Size = new System.Drawing.Size(281, 316);
+            this.ProjectListbox.TabIndex = 0;
+            this.ProjectListbox.SelectedIndexChanged += new System.EventHandler(this.ProjectListbox_SelectedIndexChanged);
+            this.ProjectListbox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ProjectListbox_MouseDoubleClick);
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(219, 336);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 1;
+            this.OKButton.Text = "&OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // ProjectListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 375);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.ProjectListbox);
+            this.Name = "ProjectListForm";
+            this.Text = "Select a Project";
+            this.Load += new System.EventHandler(this.ProjectListForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ProjectListbox;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
