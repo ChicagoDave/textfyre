63488e19201d9cb3f3f57184388c3d074e8de0e4
Chris Cavanagh <blog@chriscavanagh.com>
2010-11-24 05:18:04 +0000

    Moved HTML page "pan" area to top (so can manipulate webpage).  Improved first load so it fades onto an existing parchmnt.jpg background...

--- winphone7/Shadow/ShadowWP7/MainPage.xaml.cs
+++ winphone7/Shadow/ShadowWP7/MainPage.xaml.cs
@@ -463,9 +463,12 @@ namespace ShadowWP7
 			storageFile.CopyTextFile( "HTDocs\\intropage4.html", true );
 			storageFile.CopyTextFile( "HTDocs\\intropage5.html", true );
 			storageFile.CopyTextFile( "HTDocs\\about.html", true );
+			storageFile.CopyTextFile( "HTDocs\\blank.html", true );
 			storageFile.CopyBinaryFile( "HTDocs\\images\\oldclock.png", true );
 			storageFile.CopyBinaryFile( "HTDocs\\images\\image2.png", true );
 			storageFile.CopyBinaryFile( "HTDocs\\images\\image3.png", true );
+			storageFile.CopyBinaryFile( "HTDocs\\images\\image5.png", true );
+			storageFile.CopyBinaryFile( "HTDocs\\images\\image6.png", true );
 			storageFile.CopyBinaryFile( "HTDocs\\images\\compass.png", true );
 			storageFile.CopyBinaryFile( "HTDocs\\images\\parchmnt.jpg", true );
 			storageFile.CopyBinaryFile( "HTDocs\\images\\map-ch2.png", true );
